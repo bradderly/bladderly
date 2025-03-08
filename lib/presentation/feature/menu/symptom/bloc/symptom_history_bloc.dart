382aa245ae0d67c0ba0c59bddc42d6f7e05a4d69
eden <ydu3328@naver.com>
2025-03-08 18:10:03 +0900

    feat: [SCORE] 설문조사 데이터 초기화 로직 로그인시로 이동

--- lib/presentation/feature/menu/symptom/bloc/symptom_history_bloc.dart
+++ lib/presentation/feature/menu/symptom/bloc/symptom_history_bloc.dart
@@ -1,6 +1,5 @@
 // Package imports:
 // Project imports:
-import 'package:bladderly/domain/usecase/get_scores_server_usecase.dart';
 import 'package:bladderly/domain/usecase/get_scores_stream_usecase.dart';
 import 'package:bladderly/domain/usecase/save_score_usecase.dart';
 import 'package:bloc_concurrency/bloc_concurrency.dart';
@@ -13,36 +12,18 @@ part 'symptom_history_state.dart';
 class SymptomHistoryBloc extends Bloc<SymptomHistoryEvent, SymptomHistoryState> {
   SymptomHistoryBloc({
     required GetScoresStreamUsecase getScoresStreamUsecase,
-    required GetScoresServerUsecase getScoresServerUsecase,
     required SaveScoreUsecase saveScoreUsecase,
   })  : _getScoresStreamUsecase = getScoresStreamUsecase,
-        _getScoresServerUsecase = getScoresServerUsecase,
         _saveScoreUsecase = saveScoreUsecase,
         super(const SymptomHistoryInitial()) {
     on<SymptomHistoryEvent>(
-      (event, emit) => switch (event) {
-        SymptomHistory() => _getSymptomHistoryFromServer(event, emit),
-      },
+      (event, emit) {},
       transformer: droppable(),
     );
   }
 
   final GetScoresStreamUsecase _getScoresStreamUsecase;
-  final GetScoresServerUsecase _getScoresServerUsecase;
   final SaveScoreUsecase _saveScoreUsecase;
-
-  Future<void> _getSymptomHistoryFromServer(SymptomHistory event, Emitter<SymptomHistoryState> emit) async {
-    emit(const SymptomHistoryProgress());
-
-    final result = await _getScoresServerUsecase(userId: event.userId);
-    result.fold(
-      (exception) => emit(SymptomHistoryFailure(exception: exception)),
-      (scores) async {
-        // await _saveScoreUsecase(scores: scores);
-        emit(const SymptomHistorySuccess());
-      },
-    );
-  }
 
   Future<void> _getSymptomHistoryFromRepo(SymptomHistory event, Emitter<SymptomHistoryState> emit) async {
     emit(const SymptomHistoryProgress());
