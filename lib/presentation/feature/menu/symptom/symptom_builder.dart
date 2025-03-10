f70550e98f2cf81e6a65110476ec031ff951b8b8
김원응 <s88422001@naver.com>
2025-03-10 14:26:11 +0900

    Merge branch 'dev-woojin2' of https://github.com/bradderly/bladderly into dev-woojin2

--- lib/presentation/feature/menu/symptom/symptom_builder.dart
+++ lib/presentation/feature/menu/symptom/symptom_builder.dart
@@ -1,14 +1,9 @@
 // Flutter imports:
 // Project imports:
 import 'package:bladderly/core/di/di.dart';
-import 'package:bladderly/domain/usecase/get_scores_server_usecase.dart';
-import 'package:bladderly/domain/usecase/get_scores_stream_usecase.dart';
-import 'package:bladderly/domain/usecase/save_score_usecase.dart';
-import 'package:bladderly/presentation/feature/menu/symptom/bloc/symptom_history_bloc.dart';
 import 'package:bladderly/presentation/feature/menu/symptom/cubit/symptom_history_form_cubit.dart';
 import 'package:bladderly/presentation/feature/menu/symptom/symptom_modal.dart';
-import 'package:flutter/widgets.dart';
-// Package imports:
+import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 class SymptomBuilder extends StatelessWidget {
@@ -16,19 +11,8 @@ class SymptomBuilder extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MultiBlocProvider(
-      providers: [
-        BlocProvider<SymptomHistoryFormCubit>(
-          create: (context) => SymptomHistoryFormCubit(getScoresStreamUsecase: getIt<GetScoresStreamUsecase>()),
-        ),
-        BlocProvider<SymptomHistoryBloc>(
-          create: (context) => SymptomHistoryBloc(
-            getScoresStreamUsecase: getIt<GetScoresStreamUsecase>(),
-            getScoresServerUsecase: getIt<GetScoresServerUsecase>(),
-            saveScoreUsecase: getIt<SaveScoreUsecase>(),
-          ),
-        ),
-      ],
+    return BlocProvider<SymptomHistoryFormCubit>(
+      create: (_) => SymptomHistoryFormCubit(getScoresStreamUsecase: getIt())..setData(),
       child: const SymptomModal(),
     );
   }
