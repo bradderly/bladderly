3ff50597d7bae76548fee5b8c4b9c88b9699ad3d
eden <ydu3328@naver.com>
2025-03-15 17:01:24 +0900

    build: [PASS_CODE] 안드로이드 바이오 인증을 위해 FlutterFragmentActivity적용

--- android/app/src/main/kotlin/com/soundable/diaryandroid/us/MainActivity.kt
+++ android/app/src/main/kotlin/com/soundable/diaryandroid/us/MainActivity.kt
@@ -1,21 +1,28 @@
 package com.soundable.diaryandroid.us
 
-import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.FlutterFragmentActivity
+import io.flutter.embedding.engine.FlutterEngine
 import io.flutter.plugin.common.MethodChannel
-import android.os.Bundle
+import io.flutter.plugins.GeneratedPluginRegistrant
 
-class MainActivity: FlutterActivity(){
+class MainActivity : FlutterFragmentActivity() {
     private val CHANNEL = "com.soundable.diaryandroid.us/customBuild"
-    
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
 
-        MethodChannel(flutterEngine!!.dartExecutor, CHANNEL).setMethodCallHandler { call, result ->
-            if (call.method == "getBuildNumber") {
-                // Return the custom build number from BuildConfig
-                result.success(BuildConfig.build)  // `build` is the custom field you defined
-            } else {
-                result.notImplemented()
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+
+        flutterEngine.run {
+            GeneratedPluginRegistrant.registerWith(this)
+            MethodChannel(
+                flutterEngine.dartExecutor,
+                CHANNEL
+            ).setMethodCallHandler { call, result ->
+                if (call.method == "getBuildNumber") {
+                    // Return the custom build number from BuildConfig
+                    result.success(BuildConfig.build)  // `build` is the custom field you defined
+                } else {
+                    result.notImplemented()
+                }
             }
         }
     }
